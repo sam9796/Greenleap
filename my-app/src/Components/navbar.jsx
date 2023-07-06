97e6a32db418de5eb1de184e52a9ad6872f36a04
Shubham Jha <jhashubham976@gmail.com>
2023-07-06 17:08:42 +0530

    Final Commit

--- my-app/src/Components/navbar.jsx
+++ my-app/src/Components/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
   });
   useEffect(() => {
     const handle = async () => {
-      const resp = await fetch("http://localhost:8081/api/getData", {
+      const resp = await fetch("http://13.233.231.169/api/getData", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
